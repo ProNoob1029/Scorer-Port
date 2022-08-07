5ef8846b0428a5c5098dbb1e8000fe4b5cd8c2a8
ProNoob1029 <pronoob1029@gmail.com>
2022-08-07 16:47:15 +0300

    working on counter buttons

--- app/src/main/java/com/dragos/scorerport/feature_editor/presentation/edit/components/TextCounter.kt
+++ app/src/main/java/com/dragos/scorerport/feature_editor/presentation/edit/components/TextCounter.kt
@@ -1,10 +1,22 @@
 package com.dragos.scorerport.feature_editor.presentation.edit.components
 
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.dragos.scorerport.R
 
 @Composable
 fun TextCounter (
@@ -13,11 +25,16 @@ fun TextCounter (
     text: String,
     textStyle: TextStyle = MaterialTheme.typography.titleLarge
 ) {
-    Counter(
+    Row(
         modifier = modifier,
-        counter = counter,
-        textStyle = textStyle
-    )
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Counter(
+            counter = counter,
+            textStyle = textStyle
+        )
+        Buttons()
+    }
 }
 
 @Composable
@@ -31,4 +48,31 @@ internal fun Counter (
         text = "$counter",
         style = textStyle.copy(fontFeatureSettings = "tnum")
     )
+}
+
+@Composable
+internal fun Buttons (
+    modifier: Modifier = Modifier,
+) {
+    Row(modifier = modifier.width(IntrinsicSize.Max)) {
+        Button(
+            modifier = Modifier
+                .weight(1f)
+                /*.aspectRatio(1f)*/,
+            onClick = { /*TODO*/ },
+            contentPadding = PaddingValues(all = 0.dp)
+        ) {
+            val minus = painterResource(id = R.drawable.ic_baseline_minus_24)
+            Icon(minus , contentDescription = "Minus")
+        }
+        Button(
+            modifier = Modifier
+                .weight(1f)
+                /*.aspectRatio(1f)*/,
+            onClick = { /*TODO*/ },
+            contentPadding = PaddingValues(all = 0.dp)
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = "Add")
+        }
+    }
 }
